8c1cfa1e27c3e1ecb79a8bec83df0eaf4f976389
Gonzalo <elias.gonzaloguerrero@gmail.com>
2024-07-17 21:38:32 -0400

    installed shadcn/ui and added accordion component

--- src/app/dashboard/accordion/page.tsx
+++ src/app/dashboard/accordion/page.tsx
@@ -1,7 +1,43 @@
+import {
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
+} from "@/components/ui/accordion";
+
+const item = [
+  {
+    id: "item-1",
+    question: "Is it accessible?",
+    answer: "Yes. It adheres to the WAI-ARIA design pattern.",
+  },
+  {
+    id: "item-2",
+    question: "Is it styled?",
+    answer:
+      "Yes. It comes with default styles that matches the other components&apos; aesthetic.",
+  },
+  {
+    id: "item-3",
+    question: "Is it animated?",
+    answer:
+      "Yes. It&apos;s animated by default, but you can disable it if you prefer.",
+  },
+];
+
 export default function Page() {
   return (
     <div>
-      <h1>hello accordion</h1>
+      {item.map((res) => (
+        <Accordion type="single" collapsible className="w-full" key={res.id}>
+          <AccordionItem value={res.id} >
+            <AccordionTrigger>{res.question}</AccordionTrigger>
+            <AccordionContent>
+              {res.answer}
+            </AccordionContent>
+          </AccordionItem>
+        </Accordion>
+      ))}
     </div>
   );
 }
